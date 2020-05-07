03178f54cefab8cb5950d7d97e63520c434a4042
celson custodio <celsoncustodio@outlook.com>
2020-05-07 11:43:57 -0300

    .

---
+++ src/main/java/com/mycompany/alguma/sintatico/Principal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.alguma.sintatico;
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+/**
+ *
+ * @author celson
+ */
+public class Principal {
+    public static void main(String args[]) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        AlgumaLexer lexer = new AlgumaLexer(cs);
+
+//        // Descomentar para depurar o Léxico
+//        Token t = null;
+//        while( (t = lexer.nextToken()).getType() != Token.EOF) {
+//            System.out.println("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
+//        }
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        AlgumaParser parser = new AlgumaParser(tokens);
+        parser.programa();
+    }
+}
